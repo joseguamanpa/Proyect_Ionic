24cee3fa17aac76f43a119c98df858700e2f1efd
joseguamanp <arturoguaman96@gmail.com>
2019-08-27 09:13:44 -0500

    create home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Headers, RequestOptions } from '@angular/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  enviar() {
+    /*  	var headers = new Headers();
+    	headers.append("Accept", 'application/json');
+   		headers.append('Content-Type', 'application/json' );
+    const requestOptions = new RequestOptions({ headers: headers });
+    */
+    //
+    let postData = {
+            "id_usuario": "2"
+    }
+    let options = {
+	    headers: {
+	      'Content-Type': 'application/x-www-form-urlencoded'
+	    }
+  	};
+  	let url="https://clientes.geekslatam.com/profesionales/GeeksVersion/public/payments";
+    this.http.post(url, postData, options)
+      .subscribe(data => {
+        	console.log("envio de id_usuario");
+       }, error => {
+        	console.log(error);
+      });
+  }
 }
